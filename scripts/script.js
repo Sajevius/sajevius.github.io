84a5fd4384cee237305d9abe1fb0f956ad778703
Sajevius Crend <71295972+Sajevius@users.noreply.github.com>
2021-07-25 13:25:09 +0800

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,16 @@
+window.onScroll = function() {stickNavbar()};
+
+var navbar = document.getElementById("navbar");
+var sticky = navbar.offsetTop;
+
+function stickNavbar() {
+  if(window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky")
+  }
+}
+
 function toggleDarkMode() {
   var element = document.body;
   element.classList.toggle("dark-mode");
